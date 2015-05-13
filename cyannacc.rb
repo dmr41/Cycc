7a40e70a08be8659ce8fe187453f478fdcedded0
David Rivers <dmr41.rivers@gmail.com>
2015-05-13 10:23:41 +0200

    simple for loop to create hash - same key overwrites new key

--- cyannacc.rb
+++ cyannacc.rb
@@ -9,10 +9,21 @@ raw_file = File.open("small.txt", "r")
 file_string = raw_file.read.to_s
 
 #word to size hash
-word_size_hash = file_string.split("\n").inject(Hash.new(0)){|word, size| if(size.length >= 4); puts; word[size] = size.length; end; word}
+word_size_hash = file_string.split("\n").inject(Hash.new(0)){|word, size| if(size.length >= 4); word[size] = size.length; end; word}
+
 word_size_hash.each do |word, size|
-	puts "The word #{word} is #{size} characters long."
+	adjusted_size = size - 4 # change later
+	letter_shift_counter = 0
+
+	for i in 0..adjusted_size
+    sequence_hash[(word[letter_shift_counter..(letter_shift_counter+3)])] = word
+		if
+		letter_shift_counter+=1
+	end
+	# puts "The word #{word} is #{size} characters long."
+
 end
+puts sequence_hash
 
 stop_time = Time.now
 # puts word_size_hash
