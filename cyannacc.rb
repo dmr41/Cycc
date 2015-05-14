c5e567c7dfc4e89c6678ffe155b304a11546be5d
David Rivers <dmr41.rivers@gmail.com>
2015-05-14 12:05:40 +0200

    added input prompts to handle default dictionary or flat files - error raising on non-existant files

--- cyannacc.rb
+++ cyannacc.rb
@@ -1,14 +1,61 @@
 
-require 'open-uri'
-response = open('https://s3.amazonaws.com/cyanna-it/misc/dictionary.txt').read
+class WordAPI
+	require 'open-uri'
+	def initialize
+		@error_counter = 0
+	end
+	def api_response
+		open('https://s3.amazonaws.com/cyanna-it/misc/dictionary.txt').read
+	end
+
+	def user_file_response(user_file_path)
+		begin
+			raw_file = File.open(user_file_path, "r")
+			if raw_file
+				response = raw_file.read.to_s
+	  	else
+				raise
+			end
+		rescue
+			@error_counter += 1
+			if(@error_counter < 4)
+				puts @error_counter
+		  	puts "That file does not exist. Please check your path"
+				puts "Please enter the correct path."
+				new_user_file_path = gets.chomp
+				response = user_file_response(new_user_file_path)
+			else
+				puts "Redirecting you to our test word list after 3 attempts."
+				response = api_response
+			end
+		end
+		response
+			#
+			# raw_file = File.open(user_file_path, "r")
+			# raw_file.read.to_s
+	end
+
+end
+
+puts "This program identifies all unique 4 letter sequences from a list of words"
+puts "Would you like to use the 25K+ word default dictionary?(y/n): "
+user_data_source_choice = gets.chomp
+
+data_source = WordAPI.new
+if(user_data_source_choice[0] == "y" || user_data_source_choice[0] == "Y")
+	response = data_source.api_response
+else
+	puts "Please input the path to the file to be parsed: "
+	file_name = gets.chomp
+	response = data_source.user_file_response(file_name)
+end
+
 
 loop_init = 0
 loop_total = 100000
 start_time = Time.now
 sequence_hash = {}
-#File load
-raw_file = File.open("trial.txt", "r")
-file_string = raw_file.read.to_s
+
 
 #word to size hash
 # word_size_hash = file_string.split("\n").inject(Hash.new(0)){|word, size| if(size.length >= 4); word[size] = size.length; end; word}
@@ -27,29 +74,21 @@ word_size_hash.each do |word, size|
 		end
 		letter_shift_counter+=1
 	end
-	# puts "The word #{word} is #{size} characters long."
 
 end
 puts " Sequence\tWord"
-File.open("sequence_list.txt", 'w') { |file| file.write("Sequence") }
-File.open("word_list.txt", 'w') { |file| file.write("Word") }
+File.open("sequence_list.txt", 'w') { |file| file.write("Sequence\n") }
+File.open("word_list.txt", 'w') { |file| file.write("Word\n") }
 sequence_hash.each do |sequence, word|
 	if(word != 0)
 		File.open("sequence_list.txt", 'a') { |file| file.write("#{sequence}\n") }
 		File.open("word_list.txt", 'a') { |file| file.write("#{word}\n") }
-		puts "   #{sequence}        #{word}"
+		# puts "   #{sequence}        #{word}"
 	end
 end
 
 stop_time = Time.now
-# puts word_size_hash
-# 	counter = 0
-# word_size_hash.each do |word, size|
-# 	if size >0
-# 		counter+=1
-# 	end
-# puts counter
-# end
+
 run_time = (stop_time - start_time)
 average_time = run_time/loop_total
 puts "File-open total time of #{loop_total} runs: #{run_time} seconds"
--- simple_api.rb
+++
@@ -1,6 +0,0 @@
-
-
-require 'open-uri'
-# response = open('https://mercy-api.herokuapp.com/').read
-response = open('https://s3.amazonaws.com/cyanna-it/misc/dictionary.txt').read
-puts response
